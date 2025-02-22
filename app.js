90bc727dd9cec163adfe6003fc95f6e971e8b508
Jiaul <jiaulrbpo123@gmail.com>
2025-02-23 01:54:35 +0530

    cors issue fixed!

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 const corsOptions = {
     origin: [
-      'http://localhost:5173', 
+      'http://localhost:5174', 
       // 'http://localhost:8000'
     ],
     allowedHeaders: ['Content-Type', 'Authorization'],
