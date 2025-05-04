a3736221be87547221a6e1db206ad1a3f1e1d02d
Jiaul <jiaulrbpo123@gmail.com>
2025-05-04 21:13:36 +0530

    frontend url added for cors in app.js

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:5173'], // add other origins as needed
+  origin: ['https://mernblog.ennovatorz.com'], // add other origins as needed
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
